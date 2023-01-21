5cc7d1ee76be2e693d0f1016ed287b10a310211b
viraj205 <virajsavaliya200@gmail.com>
2023-01-22 00:00:04 +0530

    add admin name in side bar

--- controllers/admin/dashboardController.js
+++ controllers/admin/dashboardController.js
@@ -1,10 +1,19 @@
 var mongoose = require('mongoose');
+var constants = require('../../models/modelConstants');
+var sportModel = mongoose.model(constants.sportSchema);
+var teamModel = mongoose.model(constants.teamSchema);
 
 const dashboardController = {
     dashboard: async function(req, res, next){
         var responseData = {};
         responseData.pageName = 'Dashboard';
         responseData.pageTitle = process.env.APPNAME + " | " + responseData.pageName;
+
+        var sportCount = await sportModel.countDocuments({"deletedStatus": 0})
+        var teamCount = await teamModel.countDocuments({"deletedStatus": 0})
+
+        responseData.sportCount = sportCount;
+        responseData.teamCount = teamCount;
 
         res.render('pages/dashboard', responseData);
     }
